78d4527f296cca0f9a920507a22854ab66b82b6f
Paul Hostev <paul.hostev@gmail.com>
2021-03-29 02:08:04 +0200

    API visually distinguish by Icon

--- app/src/main/java/com/apzumi/challenge/ui/RepositoriesAdapter.kt
+++ app/src/main/java/com/apzumi/challenge/ui/RepositoriesAdapter.kt
@@ -25,9 +25,15 @@ class RepositoriesAdapter(
         holder.name.text = item.name
         holder.user.text = item.user
         holder.setAvatar(item.avatar)
+        holder.setRepoIcon(item.isGitHub)
     }
 
     override fun getItemCount(): Int = values.size
+
+    fun sortByAlphabet(isByAlphabet: Boolean = true) {
+        if (isByAlphabet) values.sortBy { it.name }
+        else values.sortBy { it.user }
+    }
 
     fun setList(list: List<RepositoryModel>?) {
         if (list?.isNotEmpty() ?: false) {
@@ -41,11 +47,18 @@ class RepositoriesAdapter(
         val name: TextView = view.findViewById(R.id.tv_name)
         val user: TextView = view.findViewById(R.id.tv_user)
         val avatar: AppCompatImageView = view.findViewById(R.id.iv_avatar)
+        val icon: AppCompatImageView = view.findViewById(R.id.iv_repo)
 
         fun setAvatar(url: String) = Picasso.get()
             .load(url)
             .resize(50, 50)
             .into(avatar)
 
+        fun setRepoIcon(isGitHub: Boolean) {
+            if (isGitHub) icon.setImageDrawable(itemView.context.getDrawable(R.drawable.ic_github))
+            else Picasso.get()
+                .load("https://pics.freeicons.io/uploads/icons/png/17054170491556105311-512.png")
+                .into(icon)
+        }
     }
 }
